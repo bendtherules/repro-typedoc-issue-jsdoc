7376c24a5e549584b65d4a88ff0dd85e6cf94819
Abhas <abhas@Abhass-MacBook-Pro.local>
2018-12-04 14:06:53 +0530

    Remove types.ts and usage

--- src/sip.js
+++ src/sip.js
@@ -12,21 +12,6 @@ const Calculations = {
     return maxPrice;
   },
 
-  /**
-   * Get total value of the order from order config
-   * @returns {number} - Output A
-   * @param {import('./types').OrderConfig} orderConfig - Input A
-   * @param {import('./types').SidPriceMap} sidPriceMap - Input B
-   */
-  calculateOrderTotalValue: function calculateOrderTotalValue(orderConfig, sidPriceMap) {
-    const totalValue = orderConfig.reduce((prevValue, eachSidConfig) => {
-      const sidPrice = sidPriceMap[eachSidConfig.sid];
-      const currentConfigValue = sidPrice * eachSidConfig.shares;
-      return (prevValue + currentConfigValue);
-    }, 0);
-
-    return totalValue;
-  }
 };
 
 module.exports = Calculations;
